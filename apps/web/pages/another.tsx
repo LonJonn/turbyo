ca8893b7f9cc81dc120b8f48a4c3ab7c325e8b20
LonJonn <leon.salsiccia@gmail.com>
2022-06-25 00:42:15 +1000

    update deps

---
+++ apps/web/pages/another.tsx
@@ -0,0 +1,7 @@
+export default function Another() {
+  return (
+    <div>
+      <h1>Im another page</h1>
+    </div>
+  );
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Button } from "ui";
+import { Button } from "@turbyo/ui";
+import Link from "next/link";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <Link href="/another">Go to another page</Link>
     </div>
   );
 }
