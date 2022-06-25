2c25c057aade51084a4dc9490ba84bb8e8fa546c
LonJonn <leon.salsiccia@gmail.com>
2022-06-25 22:09:22 +1000

    swap to yarn + prisma

--- apps/admin/app/routes/index.tsx
+++ apps/admin/app/routes/index.tsx
@@ -1,31 +1,32 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+
+import { prisma } from "@/utils/db.server";
+import { useLoaderData } from "@remix-run/react";
+import type { User } from "@prisma/client";
+
+type LoaderData = {
+  users: User[];
+};
+
+export const loader: LoaderFunction = async () => {
+  const users = await prisma.user.findMany();
+  return json<LoaderData>({ users });
+};
+
 export default function Index() {
+  const { users } = useLoaderData<LoaderData>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <h2>Users</h2>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} | {user.email}
+          </li>
+        ))}
       </ul>
     </div>
   );
