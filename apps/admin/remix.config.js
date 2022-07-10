0d59458761c931522b47f891ac8a3befe2f11b7d
LonJonn <leon.salsiccia@gmail.com>
2022-07-10 11:26:19 +1000

    vercel test

--- apps/admin/remix.config.js
+++ apps/admin/remix.config.js
@@ -6,7 +6,8 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: [/.*/],
+  serverDependenciesToBundle: [/@turbyo\/.*/],
+  // serverDependenciesToBundle: [/.*/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
